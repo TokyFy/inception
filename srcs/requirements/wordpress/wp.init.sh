3d195e68c87cd428ce02dd3e99684ba5daf549b7
TokyFy <tokyfydev@gmail.com>
2025-07-24 10:29:52 +0300

    Add user 2

--- srcs/requirements/wordpress/wp.init.sh
+++ srcs/requirements/wordpress/wp.init.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
-sleep 10 && cd /www  && wp core install                                         \
+sleep 10
+
+cd /www
+
+ADMIN_USER=$(head -n 1 /run/secrets/ADMIN_USER | tr -d '\n') 
+ADMIN_PASS=$(tail -n 1 /run/secrets/ADMIN_USER | tr -d '\n')
+
+wp user get "$ADMIN_USER" >/dev/null 2>&1 ||                                    \
+wp core install                                                                 \
         --url="$DOMAIN"                                                         \
         --title="franaivo"                                                      \
-        --admin_user="$(head -n 1 /run/secrets/ADMIN_USER | tr -d '\n')"        \
-        --admin_password="$(tail -n 1 /run/secrets/ADMIN_USER | tr -d '\n')"    \
-        --admin_email="change@this.com"                                                        
+        --admin_user="$ADMIN_USER"                                              \
+        --admin_password="$ADMIN_PASS"                                          \
+        --admin_email="change@this.com"      
+
+NORMAL_USER=$(head -n 1 /run/secrets/NORMAL_USER | tr -d '\n')
+NORMAL_PASS=$(tail -n 1 /run/secrets/NORMAL_USER | tr -d '\n')
+
+wp user get "$NORMAL_USER" >/dev/null 2>&1 ||                                    \
+wp user create  "$NORMAL_USER"                                                  \
+                "normal@42.fr" --role=subscriber --user_pass="$NORMAL_PASS"
 
 exec php-fpm83 -F -R
