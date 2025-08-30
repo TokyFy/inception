a33a06d9df09df89a4331e0a21b47182a61eba45
TokyFy <tokyfydev@gmail.com>
2025-08-30 07:51:00 +0300

    Add init sql

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-mariadbd &
-MARIADB_PID=$!
-
 DB_USER=$(head -n 1 /run/secrets/DB_USER | tr -d '\n') 
 DB_PASS=$(tail -n 1 /run/secrets/DB_USER | tr -d '\n')
 
@@ -12,15 +9,13 @@ sleep 5
 
 echo "INIT - Add Users / databases "
 
-mariadb <<-EOSQL
+cat > /tmp/init.sql <<-EOSQL
     CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
     CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+    CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';
     ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_PASS}';
     GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
     FLUSH PRIVILEGES;
 EOSQL
 
-kill $MARIADB_PID
-wait $MARIADB_PID
-
-exec mariadbd
+exec mariadbd --init-file=/tmp/init.sql
