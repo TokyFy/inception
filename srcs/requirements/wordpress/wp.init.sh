3d5d7d61758e2406f5be1d582c798748df47031d
TokyFy <tokyfydev@gmail.com>
2026-06-24 12:00:00 +0000

    remove usused EXPOSE

--- srcs/requirements/wordpress/wp.init.sh
+++ srcs/requirements/wordpress/wp.init.sh
@@ -22,8 +22,37 @@ wp core install                                                                 
 NORMAL_USER=$(head -n 1 /run/secrets/NORMAL_USER | tr -d '\n')
 NORMAL_PASS=$(tail -n 1 /run/secrets/NORMAL_USER | tr -d '\n')
 
-wp user get "$NORMAL_USER" >/dev/null 2>&1 ||                                    \
+wp user get "$NORMAL_USER" >/dev/null 2>&1 ||                                   \
 wp user create  "$NORMAL_USER"                                                  \
                 "normal@42.fr" --role=subscriber --user_pass="$NORMAL_PASS"
+
+wp post delete $(wp post list --post_type='post' --name='hello-world' --format=ids) --force
+
+[ "$(wp post list --post_type=post --format=ids | wc -w)" -eq 0 ] && \
+wp post create --post_title="👾Franaivo" --post_status=publish --post_type=post --post_content="$(cat <<'EOF'
+
+Hey! I’m someone who loves figuring things out — mostly by getting them wrong first. 
+I enjoy poking at everyday life like it's a mysterious puzzle box…
+except there’s no manual, and sometimes I realize I’m holding it upside down.
+
+I’m into:
+
+    *   Overthinking simple decisions like it’s an Olympic sport
+
+    *   Creating routines and then immediately rebelling against them
+
+    *   Pretending to journal, but really just doodling and writing half-thoughts
+
+    *   People-watching like I’m conducting a sociology experiment (with popcorn)
+
+    *   Getting inspired by motivational quotes… and forgetting them two minutes later
+
+    *   I believe life is best learned through trial, error, and that awkward pause when you realize you’ve been talking to yourself
+in public. I enjoy learning by doing — and occasionally by not doing, then panicking, and finally doing it last minute.
+
+This space is where I share things I notice, things I mess up, and things that make me laugh (especially when I probably shouldn’t).
+
+EOF
+)"
 
 exec php-fpm83 -F -R
