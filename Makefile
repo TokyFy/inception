4805d402f96b10eb188c67ff75ad5580545bf5d3
TokyFy <tokyfydev@gmail.com>
2026-08-02 12:00:00 +0000

    first commit

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
 up :
+	mkdir -p /home/franaivo/data/www
+	mkdir -p /home/franaivo/data/mysql
 	docker compose -f srcs/docker-compose.yml up --build
 
 down : 
 	docker compose -f srcs/docker-compose.yml down
 	@if [ -n "$$(docker volume ls -q)" ]; then docker volume rm $$(docker volume ls -q); fi
+
+fclean :
+	sudo rm -rf /home/franaivo/data
--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -10,10 +10,9 @@ sleep 5
 
 echo "INIT - Add Users / databases "
 
-
 mariadb <<-EOSQL
     CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
-    CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+    CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '$(cat /run/secrets/DB_PASSWORD | tr -d '\n')';
     GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
     FLUSH PRIVILEGES;
 EOSQL
